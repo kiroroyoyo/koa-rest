cfeb6866b9bde04fe68ea1810c345c1d6f2524d7
yoyoguo <yoyoguo@tencent.com>
2018-09-01 15:33:56 +0800

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var serve = require('koa-static');
 var route = require('koa-route');
 var koa = require('koa');
 var path = require('path');
-var app = module.exports = koa();
+var app = module.exports =  new koa();
 
 // Logger
 app.use(logger());
@@ -15,12 +15,15 @@ app.use(route.get('/', books.home));
 app.use(route.get('/books/', books.all));
 app.use(route.get('/view/books/', books.list));
 app.use(route.get('/books/:id', books.fetch));
+app.use(route.get('/booklist/:id', books.fetchByListId));
+app.use(route.get('/booklistid/:id', books.fetchByOpenId));
 app.use(route.post('/books/', books.add));
 app.use(route.put('/books/:id', books.modify));
 app.use(route.delete('/books/:id', books.remove));
 app.use(route.options('/', books.options));
 app.use(route.trace('/', books.trace));
 app.use(route.head('/', books.head));
+app.use(route.post('/userinfo/', books.fetchUserInfo));
 
 
 
--- controllers/books.js
+++ controllers/books.js
@@ -5,8 +5,11 @@ var monk = require('monk');
 var wrap = require('co-monk');
 var db = monk('localhost/library');
 var co = require('co');
+var WXBizDataCrypt = require('../utils/WXBizDataCrypt');
+var request = require('request');
 
 var books = wrap(db.get('books'));
+var lists = wrap(db.get('lists'));
 
 // From lifeofjs
 co(function * () {
@@ -53,6 +56,19 @@ module.exports.fetch = function * fetch(id,next) {
 
 };
 
+module.exports.fetchByListId = function * fetch(listid,next) {
+  if ('GET' != this.method) return yield next;
+  // Quick hack.
+  var book = yield books.find({
+    'list_id': listid
+  });
+  if (book.length === 0) {
+    this.throw(404, 'book with id = ' + listid + ' was not found');
+  }
+  this.body = yield book;
+
+};
+
 module.exports.add = function * add(data,next) {
   if ('POST' != this.method) return yield next;
   var book = yield parse(this, {
@@ -63,6 +79,57 @@ module.exports.add = function * add(data,next) {
     this.throw(405, "The book couldn't be added.");
   }
   this.body = 'Done!';
+};
+
+module.exports.fetchUserInfo = function * fetchUserInfo(data,next) {
+  if ('POST' != this.method) return yield next;
+  var userInfo = yield parse(this, {
+    limit: '100kb'
+  });
+  var appid = 'wx75d496e157ceaa7e';
+  var appsecret = '5fd699a02c81cc628f93889dab22e98e';
+  var wxurl = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + appsecret + '&js_code=' + userInfo.code + '&grant_type=authorization_code';
+  var req = '';
+  var getOpenid = () => new Promise((resolve, reject) => {
+    req = request(wxurl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        resolve(body);
+      }else{
+        reject(error);
+      }
+    });
+  })
+
+  getOpenid().then((res) => {req.end();return res }).catch(err => {req.end();console.log(err)});
+
+  var info =yield getOpenid();
+
+  // var pc = new WXBizDataCrypt(appid, userInfo.code);
+  // var dedata = pc.decryptData(userInfo.detail.encryptedData , userInfo.detail.iv);
+
+  // console.log('解密后 data: ', dedata);
+  console.log(info);
+
+  var list = yield lists.find({
+    'creater': JSON.parse(info).openid
+  });
+  if (list.length === 0) {
+    this.throw(404, 'list was not found');
+  }
+
+  this.body = {info, list};
+};
+
+module.exports.fetchByOpenId = function * fetchByOpenId(openid,next) {
+  if ('GET' != this.method) return yield next;
+  var list = yield lists.find({
+    'creater': openid
+  });
+  if (list.length === 0) {
+    this.throw(404, 'list was not found');
+  }
+
+  this.body = yield list;
 };
 
 module.exports.modify = function * modify(id,next) {
